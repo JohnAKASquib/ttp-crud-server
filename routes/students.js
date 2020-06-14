965bbd5f06e8d0956c660a8610bc3937fe785248
John Stendardo <johnakasquib96@gmail.com>
2020-06-14 19:05:28 -0400

    back end is done, all student routes work

--- routes/students.js
+++ routes/students.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const { Student } = require("../database/models");
+const { Student, Campus } = require("../database/models");
 
 /* GET all students. */
 // /api/students
@@ -8,7 +8,7 @@ router.get("/", async (req, res, next) => {
   // try to get students object from database
   try {
     // students will be the result of the Campus.findAll promise
-    const students = await Student.findAll();
+    const students = await Student.findAll({ include: Campus });
     // if students is valid, it will be sent as a json response
     console.log(students);
     res.status(200).json(students);
@@ -21,9 +21,7 @@ router.get("/", async (req, res, next) => {
 router.get("/:id", async (req, res, next) => {
   const { id } = req.params;
   try {
-    const student = await Student.findByPk(id);
-    campus = Campus.findByPk(student.campusId).name;
-    Student = { student, campusName: campus };
+    const student = await Student.findByPk(id, { include: Campus });
     console.log(student);
     res.status(200).json(student);
   } catch (err) {
@@ -35,13 +33,56 @@ router.get("/:id", async (req, res, next) => {
 //route to update a Student
 router.put("/:id", async (req, res, next) => {
   const { id } = req.params;
-  const { campusId } = req.body;
-  const updatedObj = { campusId: campusId };
+  const { firstName, lastName, email, imageUrl, gpa } = req.body;
+  const updatedObj = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    imageUrl: imageUrl,
+    gpa: gpa,
+  };
   try {
     const student = await Student.findByPk(id);
     await student.set(updatedObj);
     const updatedStudent = await student.save();
     res.status(201).send(updatedStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/", async (req, res, next) => {
+  // Take the form data from the request body
+  const { firstName, lastName, email, imageUrl, gpa } = req.body;
+  // Create a student object
+  const studentObj = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email,
+    imageUrl: imageUrl,
+    gpa: gpa,
+  };
+  try {
+    // Create a new student on the database
+    const newStudent = await Student.create(studentObj);
+    // The database would return a student
+    // send that student as a json to the client
+    res.status(201).send(newStudent);
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  // get an id for a student to delete
+  try {
+    // pass the id to the database to find student to be deleted
+    // database would either respond succcess or fail
+    const student = await Student.findByPk(id);
+    // invoke the .destroy() method on the returned student
+    await student.destroy();
+    // send a success message to the client
+    res.sendStatus(204);
   } catch (err) {
     next(err);
   }
